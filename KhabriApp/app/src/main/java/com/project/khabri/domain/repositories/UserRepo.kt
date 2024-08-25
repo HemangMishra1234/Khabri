e9815e3ba5474c20238c2abf6396086a11aa023e
Hemang Mishra <hemangmishranew@gmail.com>
2024-08-26 03:43:00 +0530

    Added files

--- KhabriApp/app/src/main/java/com/project/khabri/domain/repositories/UserRepo.kt
+++ KhabriApp/app/src/main/java/com/project/khabri/domain/repositories/UserRepo.kt
@@ -8,26 +8,30 @@ import retrofit2.HttpException
 
 class UserRepo(val auth: FirebaseAuth, val api: NewsAPI) {
     suspend fun pushUserData() {
-        val email = auth.currentUser?.email
-        val name = auth.currentUser?.displayName
-        val id = auth.currentUser?.uid
-        if (email == null || name == null || id == null) {
-            Log.i("error", "user not logged in")
-            return
-        }
-        val response = api.updateUserData(
-            UserData(
-                email = email,
-                name = name,
-                id = id,
-                isJournalist = true
+        try {
+            val email = auth.currentUser?.email
+            val name = auth.currentUser?.displayName
+            val id = auth.currentUser?.uid
+            if (email == null || name == null || id == null) {
+                Log.i("error", "user not logged in")
+                return
+            }
+            val response = api.updateUserData(
+                UserData(
+                    email = email,
+                    name = name,
+                    id = id,
+                    isJournalist = true
+                )
             )
-        )
-        Log.i("response", "$response")
-        if (response.isSuccessful.not()) {
-            throw HttpException(response)
+            Log.i("response", "$response")
+            if (response.isSuccessful.not()) {
+                throw HttpException(response)
+            }
+            val articleList = response.body() ?: throw Exception()
+            Log.i("fetched", "$articleList")
+        } catch (e: Exception) {
+            Log.i("error", "$e")
         }
-        val articleList = response.body() ?: throw Exception()
-        Log.i("fetched", "$articleList")
     }
 }
