b90e7987a2f0999320b3531e9f780635d4bc9d0d
Choco-10 <harshiv710@gmail.com>
2024-08-26 00:16:51 +0530

    change

--- KhabriDjango/khabri/news/admin.py
+++ KhabriDjango/khabri/news/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from .models import News
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'published_at', 'source_name')
+    list_display = ('title', 'published_at', 'source_name', 'is_active')  # Add 'is_active' to the display list
     search_fields = ('title', 'description', 'content')
     list_per_page = 100  # Set the number of items per page
+    list_filter = ('is_active',)  # Optionally add filter for 'is_active'
 
 admin.site.register(News, NewsAdmin)
