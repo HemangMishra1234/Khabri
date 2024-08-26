fdd54a1dea8a509672effa9e69be1f152669a2b2
Hemang Mishra <hemangmishranew@gmail.com>
2024-08-26 16:41:31 +0530

    added apps

--- KhabriApp/app/src/main/java/com/project/khabri/data/remote/HttpRoutes.kt
+++ KhabriApp/app/src/main/java/com/project/khabri/data/remote/HttpRoutes.kt
@@ -1,7 +1,7 @@
 package com.project.khabri.data.remote
 
 object HttpRoutes {
-    const val BASE_URL = "https://rndgb-2401-4900-51c8-df10-ea73-ffb2-a9b0-8996.a.free.pinggy.link"
+    const val BASE_URL = "https://rnivl-2401-4900-51c8-df10-5c7e-b2e3-6e1f-d810.a.free.pinggy.link"
     const val POSTS = "$BASE_URL/posts"
 }
 
